37d3a2d869502271c93e9f747ed0e2de4930ce10
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-03-21 15:52:31 -0400

    add spinner and ability to create accounts

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Button, Input } from './common';
+import { Text } from 'react-native';
+import { Card, CardSection, Button, Input, Spinner } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-    state = { email: '' };
+    state = { email: '', password: '', error: '', loading: false };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        this.setState({ error: '', loading: true });
+        console.log(firebase.apps);
+        console.log("Entering on button press");
+        console.log(firebase.auth());
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(this.onLoginFailure.bind(this));
+    }
+
+    onLoginSuccess(e) {
+        console.log("Success")
+        console.log(e);
+        this.setState({ email: '', password: '', error: '', loading: false });
+    }
+
+    onLoginFailure(e) {
+        const { email, password } = this.state;
+        console.log("Inside first error, attempting to create email")
+        console.log(e);
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch((e) => {
+                console.log("Inside second error")
+                console.log(e);
+                this.setState({ error: 'Authentication Failed', loading: false });
+            });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="large" />
+        }
+
+        return (
+            <CardSection>
+                <Button onPress={this.onButtonPress.bind(this)}>
+                    Log In
+            </Button>
+            </CardSection>
+        )
+    }
     render() {
         return (
             <Card>
@@ -10,17 +56,35 @@ class LoginForm extends Component {
                     <Input
                         label="Email"
                         placeholder="user@gmail.com"
+                        secureTextEntry={false}
                         value={this.state.email}
                         onChangeText={email => { this.setState({ email }); }}
                     />
                 </CardSection>
-                <CardSection></CardSection>
                 <CardSection>
-                    <Button>Log In</Button>
+                    <Input
+                        label="Password"
+                        placeholder="***********"
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={password => { this.setState({ password }); }}
+                    />
                 </CardSection>
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+                {this.renderButton()}
             </Card>
         )
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
 
 export default LoginForm;
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ placeholder, label, value, onChangeText }) => {
+const Input = ({ placeholder, label, value, onChangeText, secureTextEntry }) => {
     const { labelStyle, containerStyle, inputStyle } = styles;
     return (
         <View style={containerStyle}>
             <Text style={labelStyle}>{label}</Text>
             <TextInput
+                secureTextEntry={secureTextEntry}
                 placeholder={placeholder}
                 autoCorrect={false}
                 style={inputStyle}
