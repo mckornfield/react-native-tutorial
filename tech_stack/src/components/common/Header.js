c7282f4427ef037bb6dfd3b4ceefd7b374b7adca
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-03-22 10:17:44 -0400

    add files from other project

---
+++ tech_stack/src/components/common/Header.js
@@ -0,0 +1,36 @@
+// import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a component
+const Header = (props) => {
+    const { textStyle, viewStyle } = style;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const style = {
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f8f8f8',
+        height: 60,
+        paddingTop: 15,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+// make the component available to other parts of the app
+export { Header };
