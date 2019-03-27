a29a1482227aef4c5f603a1df92e9de8f7c15f1f
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-03-27 14:03:46 -0400

    remove firebase info

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,17 +5,10 @@ import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import data from './config.json';
 
 class App extends Component {
     componentWillMount() {
-        const data = {
-            apiKey: "AIzaSyCMPAJV14lvEfZ9JJiDkM1RgLkp6UVxG-Y",
-            authDomain: "manager-a213c.firebaseapp.com",
-            databaseURL: "https://manager-a213c.firebaseio.com",
-            projectId: "manager-a213c",
-            storageBucket: "manager-a213c.appspot.com",
-            messagingSenderId: "896042165421"
-        };
         const val = firebase.initializeApp(data);
         console.log(val);
     }
